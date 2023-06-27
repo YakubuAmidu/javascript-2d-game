68fca2b4f0788e51f5d5b7957ec24c07e67caf52
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-27 09:42:34 -0700

    Initial commit

--- script.js
+++ script.js
@@ -32,7 +32,19 @@ window.addEventListener('load', function(){
     };
 
     class Player {
+       constructor(gameWidth, gameHeight){
+          this.gameWidth = gameWidth;
+          this.gameHeight = gameHeight;
+          this.width = 200;
+          this.height = 200;
+          this.x = 0;
+          this.y = this.gameHeight - this.height;
+       }
 
+       draw(context){
+           context.fillStyle = 'white';
+           context.fillRect(this.x, this.y, this.width, this.height);
+       }
     }
 
     class Background {
@@ -52,6 +64,8 @@ window.addEventListener('load', function(){
     }
 
     const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+    player.draw(ctx);
 
     function animate() {
 
