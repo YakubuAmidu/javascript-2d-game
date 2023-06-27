c411918c44f588d22618e25c7f21acae2225b877
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-27 09:14:26 -0700

    Initial commit

--- script.js
+++ script.js
@@ -10,10 +10,23 @@ window.addEventListener('load', function(){
         constructor(){
             this.keys = [];
             window.addEventListener('keydown', e => {
-                if(e.key == 'ArrowDown'){
+                if ((e.key === 'ArrowDown' || 
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight' ) 
+                    && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key);
                 }
-                console.log('E.KEY: ', e.key, 'THIS.KEYS: ', this.keys);
+            });
+
+            window.addEventListener('keyup', e => {
+                if ((e.key === 'ArrowDown' ||
+                     e.key === 'ArrowUp' ||
+                     e.key === 'ArrowLeft' ||
+                     e.key === 'ArrowRight'))
+                     {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
             })
         }
     };
