8403f91e4e106465054e46d7b677e8cce0c65136
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-27 11:06:36 -0700

    Initial commit

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ window.addEventListener('load', function(){
           this.frameX = 0;
           this.frameY = 0;
           this.speed = 0;
+          this.vy = 0;
+          this.weight = 1;
        }
 
        draw(context){
@@ -52,18 +54,34 @@ window.addEventListener('load', function(){
        }
 
        update(input){
-        // Horizontal movement
-          this.x += this.speed;
           if(input.keys.indexOf('ArrowRight') > - 1){
             this.speed = 5;
           } else if (input.keys.indexOf('ArrowLeft') > - 1){
             this.speed = -5;
+          } else if (input.keys.indexOf('ArrowUp') > - 1 && this.onGround()) {
+             this.vy -= 32;
           } else {
             this.speed = 0;
           }
-
+        
+          // Horizontal movement
+          this.x += this.speed;
           if (this.x < 0) this.x = 0;
           else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+
+          // Vertical movement
+         this.y += this.vy;
+         if (!this.onGround()){
+            this.vy += this.weight;
+         } else {
+            this.vy = 0;
+         }
+
+         if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+       }
+
+       onGround(){
+         return this.y >= this.gameHeight - this.height;
        }
     }
 
