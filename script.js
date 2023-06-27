530cfab026eec6f744c48abf3bfbfb7b5887e3b5
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-26 18:10:29 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+    console.log('Hello...');
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = 400;
+    canvas.height = 500;
+
+    
+})
