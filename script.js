44b4665a62de5d59db415a0a6a74f3365f61e9e9
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-01 09:55:34 -0700

    Initial commit

--- script.js
+++ script.js
@@ -87,6 +87,11 @@ window.addEventListener('load', function(){
        }
 
        draw(context){
+           context.lineWidth = 5;
+           context.strokeStyle = 'white';
+           context.beginPath();
+           context.arc(this.x + this.width / 2, this.y + this.height / 2,  this.width / 2, 0, Math.PI * 2);
+           context.stroke();
            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
        }
 
