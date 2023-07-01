9b1141da073ae6dbcce0a5201f346a3cad12387f
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-01 10:01:37 -0700

    Initial commit

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -221,6 +221,11 @@ window.addEventListener('load', function () {
     _createClass(Enemy, [{
       key: "draw",
       value: function draw(context) {
+        context.lineWidth = 5;
+        context.strokeStyle = 'white';
+        context.beginPath();
+        context.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2);
+        context.stroke();
         context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
       }
     }, {
